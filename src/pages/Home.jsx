8c23d1fd32f01682c9d1514374251e9a11f21853
ceddevstudent89 <cedstudent89@gmail.com>
2024-02-07 14:25:41 +0100

    Stylisation page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,15 @@ import Banner from "../components/Banner";
 import Wrapper from "../components/Wrapper";
 import { Link } from "react-router-dom";
 import data from "../data/data.json";
-import "../index.css";
+import styled from "styled-components";
+
+const CardsList = styled.div`
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
+  gap: 50px 60px;
+  justify-content: center;
+`;
 
 export default function Home() {
   const title = "Chez vous, partout et ailleurs";
@@ -20,7 +28,7 @@ export default function Home() {
     <div>
       <Banner title={title} backgroundImage="../src/assets/cote.png" />
       <Wrapper>
-        <div className="cards-list">{dataElements}</div>
+        <CardsList>{dataElements}</CardsList>
       </Wrapper>
     </div>
   );
